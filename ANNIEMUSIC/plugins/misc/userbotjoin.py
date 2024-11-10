db396c708d687fbd1d5cc195786433fee2f8db88
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-11-10 22:20:25 +0530

    Update userbotjoin.py

--- ANNIEMUSIC/plugins/misc/userbotjoin.py
+++ ANNIEMUSIC/plugins/misc/userbotjoin.py
@@ -188,7 +188,7 @@ async def leave_all(client, message):
     try:
         userbot = await get_assistant(message.chat.id)
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002024032988:
+            if dialog.chat.id == -1002122538649:
                 continue
             try:
                 await userbot.leave_chat(dialog.chat.id)
